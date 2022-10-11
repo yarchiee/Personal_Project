dcd6a19826499c6b5e12d4f1ae876c52abf06a79
yarchiee <yachi98159@gmail.com>
2022-10-11 22:16:05 +0800

    issue page layout wip

--- src/pages/IssuePage/IssuePage.tsx
+++ src/pages/IssuePage/IssuePage.tsx
@@ -1,9 +1,76 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import SideBarArea from "../NewIssuePage/SideBarArea";
+import CreateArea from "../NewIssuePage/CreateArea";
+import SubmitBtn from "../NewIssuePage/SubmitBtn";
 
-function IssuePage({}) {
+function IssuePage({
+  labelData,
+  isAssignee,
+  whoIsAssignee,
+  setWhoIsAssignee,
+  selectdLabel,
+  setSelectedLabel,
+  newCreateIssue,
+  check,
+  setCheck,
+  selectedAvatarUrl,
+  setSelectedAvatarUrl,
+  selectedLabelColor,
+  setSelectedLabelColor,
+  typeIssuelName,
+  setTypeIssueName,
+  leaveComment,
+  setLeaveComment,
+  postCreateIssue,
+}) {
   let { userId } = useParams();
-  return <></>;
+  return (
+    <>
+      <div className="mt-[24px] h-[114px] border border-solid  border-b-[hsla(210,18%,87%,1)] border-l-0 border-r-0  border-t-0 px-[16px] md:mx-[32px] xl:mx-[119.6px] ">
+        114
+      </div>
+      <div className=" px-[16px] md:flex  md:px-[32px] xl:mx-[119.6px]">
+        <img
+          src="https://avatars.githubusercontent.com/u/105163825?s=80&v=4"
+          alt=""
+          className=" hidden md:rounded-[50%] md:w-[40px] md:h-[40px] md:block md:mr-[15px] md:border md:border-solid md:border-[#d0d7de]"
+        />
+
+        <CreateArea
+          typeIssuelName={typeIssuelName}
+          setTypeIssueName={setTypeIssueName}
+          leaveComment={leaveComment}
+          setLeaveComment={setLeaveComment}
+          newCreateIssue={newCreateIssue}
+          postCreateIssue={postCreateIssue}
+        />
+        <SideBarArea
+          labelData={labelData}
+          isAssignee={isAssignee}
+          whoIsAssignee={whoIsAssignee}
+          setWhoIsAssignee={setWhoIsAssignee}
+          selectdLabel={selectdLabel}
+          setSelectedLabel={setSelectedLabel}
+          newCreateIssue={newCreateIssue}
+          check={check}
+          setCheck={setCheck}
+          selectedAvatarUrl={selectedAvatarUrl}
+          setSelectedAvatarUrl={setSelectedAvatarUrl}
+          selectedLabelColor={selectedLabelColor}
+          setSelectedLabelColor={setSelectedLabelColor}
+        />
+        <div className="block md:hidden">
+          <SubmitBtn
+            disabled={true}
+            onClick
+            postCreateIssue={postCreateIssue}
+            typeIssuelName={typeIssuelName}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default IssuePage;
--- src/pages/IssuePage/index.tsx
+++ src/pages/IssuePage/index.tsx
@@ -5,6 +5,26 @@ function IssuePageMain() {
   const [timeLineEvent, setTimeLineEvent] = useState([]);
   const [perIssueData, setPerIssueData] = useState({});
   const [labelData, setLabelData] = useState([]);
+  const [isAssignee, setIsAssignee] = useState([]);
+  const [typeIssuelName, setTypeIssueName] = useState("");
+  const [leaveComment, setLeaveComment] = useState("");
+  const [whoIsAssignee, setWhoIsAssignee] = useState("");
+  const [selectdLabel, setSelectedLabel] = useState("");
+  const [selectedAvatarUrl, setSelectedAvatarUrl] = useState("");
+  const [selectedLabelColor, setSelectedLabelColor] = useState("");
+  const [check, setCheck] = useState<string[]>([]);
+  const newCreateIssue = {
+    title: typeIssuelName,
+    body: leaveComment,
+    assignees: [...whoIsAssignee],
+    labels: [...selectdLabel],
+    avatarUrl: [...selectedAvatarUrl],
+    labelColor: [...selectedLabelColor],
+    checkone: [...check],
+  };
+  const postCreateIssue = async () => {
+    await api.createIssue(newCreateIssue);
+  };
   const fetchGetLabelData = () => {
     api.listLabelAll().then((res) => {
       setLabelData(res);
@@ -30,7 +50,26 @@ function IssuePageMain() {
 
   return (
     <>
-      <IssuePage />
+      <IssuePage
+        labelData={labelData}
+        isAssignee={isAssignee}
+        whoIsAssignee={whoIsAssignee}
+        setWhoIsAssignee={setWhoIsAssignee}
+        selectdLabel={selectdLabel}
+        setSelectedLabel={setSelectedLabel}
+        newCreateIssue={newCreateIssue}
+        check={check}
+        setCheck={setCheck}
+        selectedAvatarUrl={selectedAvatarUrl}
+        setSelectedAvatarUrl={setSelectedAvatarUrl}
+        selectedLabelColor={selectedLabelColor}
+        setSelectedLabelColor={setSelectedLabelColor}
+        typeIssuelName={typeIssuelName}
+        setTypeIssueName={setTypeIssueName}
+        leaveComment={leaveComment}
+        setLeaveComment={setLeaveComment}
+        postCreateIssue={postCreateIssue}
+      />
     </>
   );
 }
